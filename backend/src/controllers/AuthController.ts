83b5c3d1ff054341a02ae0ab87dc3460499427f3
Geraldine743 <egeraldine2@gmail.com>
2026-04-21 16:00:11 +0200

    add function login in AuthController.ts

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from 'express';
 import { UserModel } from '../models/UserModel.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export class AuthController {
     static async register(req: Request, res: Response) {
@@ -24,6 +25,39 @@ export class AuthController {
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: "Erreur lors de l'inscription" });
+        }
+    }
+
+    static async login(req: Request, res: Response) {
+        try {
+            const { email, password } = req.body;
+
+            const user = await UserModel.findByEmail(email);
+            if (!user) {
+                return res.status(401).json({ message: "Mot de passe ou identifiant incorrect" });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(401).json({ message: "Mot de passe ou identifiant incorrect" });
+            }
+
+            const secret = process.env.JWT_SECRET || 'mon_secret_ultra_sur';
+            const token = jwt.sign(
+                { id: user.id, email: user.email }, 
+                secret, 
+                { expiresIn: '24h' }
+            );
+
+            res.json({
+                success: true,
+                token,
+                user: { name: user.name, email: user.email }
+            });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Erreur lors de la connexion" });
         }
     }
 }
