3ad32b10e27b7b9870b43c7f560dada6ec5d336f
Geraldine743 <egeraldine2@gmail.com>
2026-04-21 17:43:41 +0200

    add SurpriseModel.ts

---
+++ backend/src/models/SurpriseModel.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+
+const surpriseSchema = new Schema({
+    day: { type: Number, required: true, unique: true },
+    type: { type: String, required: true }, // 'text', 'image', 'video'
+    content: { type: String, required: true },
+    title: { type: String }
+});
+
+export const SurpriseModel = model('Surprise', surpriseSchema);
