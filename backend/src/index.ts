c5b6deb0721f9034814f938ad52095441f58f847
Geraldine743 <egeraldine2@gmail.com>
2026-04-21 14:46:42 +0200

    update config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,23 @@
-import  express from 'express';
+import express from 'express';
+import cors from 'cors';
+import pool from './db.js'; // Notez le .js à la fin pour que Node.js le trouve bien !
 
 const app = express();
+app.use(cors()); // Autorise le Frontend (Vue.js) à parler au Backend (Node.js)
+
 const PORT = 3000;
+
+// Notre première route de test pour voir les calendriers
+app.get('/api/admin/calendars', async (req, res) => {
+    try {
+        // Le Pool prête une connexion automatiquement, lance la requête, puis la récupère
+        const [rows] = await pool.query('SELECT * FROM calendars ORDER BY created_at DESC');
+        res.json({ success: true, data: rows });
+    } catch (error) {
+        console.error("Erreur de base de données :", error);
+        res.status(500).json({ success: false, message: "Erreur serveur" });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send("L'API du calendrier de Michel est en ligne !");
