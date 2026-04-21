bb2a4dbfe497d6b8e649cba77498eb444aa7283e
Geraldine743 <egeraldine2@gmail.com>
2026-04-21 15:53:00 +0200

    update index.ts with route register

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import pool from './db.js'; // Notez le .js à la fin pour que Node.js le trouve bien !
+import { AuthController } from './controllers/AuthController.js';
 
 const app = express();
-app.use(cors()); // Autorise le Frontend (Vue.js) à parler au Backend (Node.js)
+app.use(express.json());
+app.use(cors()); 
 
 const PORT = 3000;
 
-// Notre première route de test pour voir les calendriers
-app.get('/api/admin/calendars', async (req, res) => {
-    try {
-        // Le Pool prête une connexion automatiquement, lance la requête, puis la récupère
-        const [rows] = await pool.query('SELECT * FROM calendars ORDER BY created_at DESC');
-        res.json({ success: true, data: rows });
-    } catch (error) {
-        console.error("Erreur de base de données :", error);
-        res.status(500).json({ success: false, message: "Erreur serveur" });
-    }
-});
+app.post('/api/auth/register', AuthController.register);
 
 app.get('/', (req, res) => {
     res.send("L'API du calendrier de Michel est en ligne !");
 });
+
 
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
